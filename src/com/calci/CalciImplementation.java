ae0d23a9ed1d77c581971b0afe8adbc1d08f892f
Balingam12 <burryrakshitha12@gmail.com>
2025-11-18 18:54:43 +0530

    added implementation

---
+++ src/com/calci/CalciImplementation.java
@@ -0,0 +1,29 @@
+package com.calci;
+
+public class CalciImplementation implements Calci {
+
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int multi(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int div(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
