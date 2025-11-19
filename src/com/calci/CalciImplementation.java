77ce9cbddc14003a3fae9eef98611ea31a911121
AdarshNetha <abhiadarshnetha849@gmail.com>
2025-11-19 18:51:49 +0530

    added add with 3 parameters

--- src/com/calci/CalciImplementation.java
+++ src/com/calci/CalciImplementation.java
@@ -22,16 +22,17 @@ public class CalciImplementation implements Calci {
 
 	@Override
 	public int div(int a, int b) {
-<<<<<<< HEAD
-//		this adarsh code
-=======
 
->>>>>>> 177f1f4f8e37741dcb3fd301ac136db99d5f2071
 		return a/b;
 	}
 	
 	public int mod(int a,int b) {
 		return a%b;
+	}
+	
+	public int add(int a,int b, int c)
+	{
+		return a+b+c;
 	}
 
 }
